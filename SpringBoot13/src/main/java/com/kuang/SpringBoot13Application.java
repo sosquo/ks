bb3b6b03d1253530bea00f53268be2f50a582f1e
zhucm <506753586@qq.com>
2023-10-11 17:15:56 +0800

    国际化功能 locale

---
+++ SpringBoot13/src/main/java/com/kuang/SpringBoot13Application.java
@@ -0,0 +1,13 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot13Application.class, args);
+    }
+
+}
