435def1e75e651eb593346da3bd5c6a78f7f3cb7
zhucm <506753586@qq.com>
2023-10-18 15:49:17 +0800

    SpringBoot18 SpringSecurity

---
+++ SpringBoot18/src/main/java/com/kuang/SpringBoot18Application.java
@@ -0,0 +1,13 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot18Application.class, args);
+    }
+
+}
